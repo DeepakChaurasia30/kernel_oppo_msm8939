0a32511236642059fac3e0d1c4bf029ffa50a6e4
Jebaitedneko <16777012+Jebaitedneko@users.noreply.github.com>
2020-09-21 17:02:55 +0530

    build: introduce fully automated building script READ MORE...
    * ccache must be installed
    
    * python2-virtualenv must be installed

---
+++ build.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+tcdir=${HOME}/android/TOOLS/GCC
+
+virtualenv2 venv
+source venv/bin/activate
+
+[ -d "out" ] && rm -rf out && mkdir -p out || mkdir -p out
+[ -d "venv" ] && rm -rf venv && mkdir -p venv || mkdir -p venv
+
+make O=out ARCH=arm64 lineageos_a37f_defconfig
+
+[ -d $tcdir ] && \
+echo "ARM64 TC Present." || \
+echo "ARM64 TC Not Present. Downloading..." | \
+git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 $tcdir/los-4.9-64
+
+[ -d $tcdir ] && \
+echo "ARM32 TC Present." || \
+echo "ARM32 TC Not Present. Downloading..." | \
+git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 $tcdir/los-4.9-32
+
+PATH="$tcdir/los-4.9-64/bin:$tcdir/los-4.9-32/bin:${PATH}" \
+make    O=out \
+        ARCH=arm64 \
+        CC="ccache $tcdir/los-4.9-64/bin/aarch64-linux-android-gcc" \
+        CROSS_COMPILE=aarch64-linux-android- \
+        CROSS_COMPILE_ARM32=arm-linux-androideabi- \
+        CONFIG_NO_ERROR_ON_MISMATCH=y \
+        CONFIG_DEBUG_SECTION_MISMATCH=y \
+        -j$(nproc --all)
+
+cc anykernel3/dtbtool.c -o out/arch/arm64/boot/dts/dtbtool
+cd out/arch/arm64/boot/dts
+./dtbtool -s 2048 -o dt.img
+cd ../../../../..
+
+fmt=`date +%d\.%m\.%Y_%H\:%M\:%S`
+pre=A37F
+cp out/arch/arm64/boot/dts/dt.img anykernel3 && cp out/arch/arm64/boot/Image anykernel3 && cd anykernel3
+zip -r ${pre}_KERNEL_${fmt}.zip . -x 'LICENSE' 'README.md' 'dtbtool.c'
+mv *.zip ../out
