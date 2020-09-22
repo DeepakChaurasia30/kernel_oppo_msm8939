760bb3a62f055c26d8572f7fb62363ed90441b37
Jebaitedneko <16777012+Jebaitedneko@users.noreply.github.com>
2020-09-22 17:26:24 +0530

    build: cleanup script

--- build.sh
+++ build.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 BUILD_START=$(date +"%s")
-
 tcdir=${HOME}/android/TOOLS/GCC
 
 [ -d "out" ] && rm -rf out && mkdir -p out || mkdir -p out
-[ -d "venv" ] && rm -rf venv && mkdir -p venv || mkdir -p venv
-
-virtualenv2 venv
-source venv/bin/activate
-
-make O=out ARCH=arm64 lineageos_a37f_defconfig
 
 [ -d $tcdir ] && \
 echo "ARM64 TC Present." || \
@@ -21,6 +14,8 @@ echo "ARM32 TC Present." || \
 echo "ARM32 TC Not Present. Downloading..." | \
 git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 $tcdir/los-4.9-32
 
+make O=out ARCH=arm64 lineageos_a37f_defconfig
+
 PATH="$tcdir/los-4.9-64/bin:$tcdir/los-4.9-32/bin:${PATH}" \
 make    O=out \
         ARCH=arm64 \
@@ -31,18 +26,12 @@ make    O=out \
         CONFIG_DEBUG_SECTION_MISMATCH=y \
         -j$(nproc --all) || exit
 
-cc anykernel3/dtbtool.c -o out/arch/arm64/boot/dts/dtbtool
-cd out/arch/arm64/boot/dts
-./dtbtool -s 2048 -o dt.img
-cd ../../../../..
+cp out/arch/arm64/boot/Image anykernel3
 
-fmt=`date +%d\.%m\.%Y_%H\:%M\:%S`
-pre=A37F
-cp out/arch/arm64/boot/dts/dt.img anykernel3 && cp out/arch/arm64/boot/Image anykernel3 && cd anykernel3
-# cp /path/to/custom/dt.img anykernel3
-cp out/arch/arm64/boot/Image anykernel3 && cd anykernel3
-zip -r ${pre}_KERNEL_${fmt}.zip . -x 'LICENSE' 'README.md' 'dtbtool.c'
-mv *.zip ../out
+cc anykernel3/dtbtool.c -o out/arch/arm64/boot/dts/dtbtool
+( cd out/arch/arm64/boot/dts; dtbtool -v -s 2048 -o dt.img )
+
+( cd anykernel3; zip -r ../out/A37F_KERNEL_`date +%d\.%m\.%Y_%H\:%M\:%S`.zip . -x 'LICENSE' 'README.md' 'dtbtool.c' )
 
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
